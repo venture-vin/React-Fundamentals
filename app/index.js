967c2d2ea004a27aa41cd2198946734cf75d02e2
Olya Royall <email": null>
2016-06-24 17:34:21 -0700

    create several components to render Avatars

--- app/index.js
+++ app/index.js
@@ -1,15 +1,50 @@
+var USER_DATA = {
+  name: "Olya Royall",
+  username: 'venture-vin',
+  image: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/5/005/0b0/247/0efb167.jpg'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
   render: function() {
-    return (
-      <div> Hello World </div>
-      )
+    return <img src={this.props.imageUrl} style= {{heigh: 100, width: 100}} />
   }
 });
 
+var ProfileLink = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+        {this.props.username}
+        </a>
+      </div>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function() {
+    return <div>{this.props.name}</div>
+  }
+})
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+    <div>
+      <ProfilePic imageUrl={this.props.user.image} />
+      <ProfileName name={this.props.user.name} />
+      <ProfileLink username={this.props.user.username} />
+    </div>
+    )
+  }
+})
+
+
 ReactDOM.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 )
