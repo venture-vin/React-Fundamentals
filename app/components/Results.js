f0a0e0f27251b7269c0d8b7b2cb9ea0586e12e97
Olya Royall <email": null>
2016-06-25 21:40:11 -0700

    update some logic for ties and loading

--- app/components/Results.js
+++ app/components/Results.js
@@ -5,7 +5,31 @@ var Link = require('react-router').Link;
 var UserDetails = require('../components/UserDetails');
 var UserDetailsWrapper = require('../components/UserDetailsWrapper');
 
+function StartOver (){
+  return (
+    <div className='col-sm-12' style={styles.space}>
+      <Link to='/playerOne'>
+        <button type='button' className='btn btn-lg btn-danger'>Start Over</button>
+      </Link>
+    </div>
+  )
+}
+
 function Results (props) {
+  if (props.isLoading === true) {
+  return (
+    <p>LOADING!</p>
+  )
+}
+
+  if (props.scores[0] === props.scores[1]) {
+    return (
+      <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+        <h1>It's a tie</h1>
+        <StartOver />
+      </div>
+    )
+  }
   var winningIndex = props.scores[0] > props.scores[1] ? 0 : 1;
   var losingIndex = winningIndex === 0 ? 1 : 0;
   return (
@@ -19,11 +43,7 @@ function Results (props) {
       <UserDetails score={props.scores[losingIndex]} info={props.playersInfo[losingIndex]} />
         </UserDetailsWrapper>
     </div>
-    <div className='col-sm-12' style={styles.space}>
-      <Link to='/playerOne'>
-        <button type='button' className='btn btn-lg btn-danger'>Start Over</button>
-      </Link>
-    </div>
+    <StartOver />
   </div>
   )
 }
