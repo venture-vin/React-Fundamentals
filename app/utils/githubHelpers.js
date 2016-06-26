1d9a46678a5bf18803d4a746e4b3fe13adec3524
Olya Royall <email": null>
2016-06-25 21:13:29 -0700

    update battle helper method

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -8,6 +8,34 @@ var axios = require('axios');
 function getUserInfo (username){
   return axios.get('https://api.github.com/users/' + username)
 }
+
+function getRepos(username) {
+  return axios.get('https://api.github.com/users/' + username+ '/repos' + '&per_page=100');
+}
+
+function getTotalStars (repos){
+  return repos.data.reduce(function(prev, current) {
+    return prev + current.stargazers_count
+  }, 0)
+}
+function getPlayersData (player){
+  return getRepos(player.login)
+    .then(getTotalStars)
+    .then(function (totalStars) {
+      return {
+        followers: player.followers,
+        totalStars: totalStars
+      }
+    })
+}
+
+function calculateScores(players){
+  return [
+    players[0].followers*3 + players[0].totalStars,
+    players[1].followers*3 + players[1].totalStars
+  ]
+}
+
 var helpers = {
   getPlayersInfo: function(players){
     return axios.all(players.map(function (username) {
@@ -17,6 +45,16 @@ var helpers = {
         return user.data;
       })
     }).catch(function(error){
+      console.warn('Error in getPlayersInfo', err);
+    })
+  },
+
+  battle(players){
+    var playerOneData = getPlayersData(player[0]);
+    var playerTwoData = getPlayersData(player[1]);
+    return axios.all([playerOneData, playerTwoData])
+    .then(calculateScores)
+    .catch(function(error){
       console.warn('Error in getPlayersInfo', err);
     })
   }
